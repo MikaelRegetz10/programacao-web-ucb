7d3f14d709c148cbaf9e5aec38c2f0ca102c367d
MikaelRegetz10 <mikaelregetz@gmail.com>
2025-05-20 10:10:10 -0300

    Validacoes do formulario

---
+++ atividade 5/router/agendamentoRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const agendamentoController = require('./controller/agendamentoController');
+
+router.get('/', agendamentoController.getIndexView);
+
+router.post('/agendar_consulta', agendamentoController.agendarConsulta);
+
+
+module.export = router
